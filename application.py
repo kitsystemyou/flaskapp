dbb96541dbb9ce8c702825c20108212a2ad6ab1f
kitsystemyou <kitsystemyou@gmail.com>
2020-02-22 07:10:32 +0900

    testpage

--- application.py
+++ application.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    print("Handling request to home page.")
-    return "Hello Azure"
+@app.route('/')
+def index():
+    title = "ようこそ"
+    message = "Hi! this is App test page"
+    return render_template('index.html', message=message, title=title)
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    title = "こんにちは"
+    if request.method == 'POST':
+        # リクエストフォームから「名前」を取得して
+        name = request.form['name']
+        # index.html をレンダリングする
+        return render_template('index.html',
+                               name=name, title=title)
+    else:
+        # エラーなどでリダイレクトしたい場合はこんな感じで
+        return redirect(url_for('index'))
