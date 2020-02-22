5c7be68b1e1e8d44b9d55d52d10324b91bfeb81d
kitsystemyou <kitsystemyou@gmail.com>
2020-02-23 00:09:38 +0900

    change path post to bow

--- application.py
+++ application.py
@@ -3,19 +3,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # set title and opening message
     title = "ようこそ"
     message = "Hi! this is App test page"
     return render_template('index.html', message=message, title=title)
 
-@app.route('/post', methods=['GET', 'POST'])
+@app.route('/bow', methods=['GET', 'POST'])
 def post():
     title = "こんにちは"
     if request.method == 'POST':
-        # リクエストフォームから「名前」を取得して
+        # get "name" from request form
         name = request.form['name']
-        # index.html をレンダリングする
+        # render index.html
         return render_template('index.html',
                                name=name, title=title)
     else:
-        # エラーなどでリダイレクトしたい場合はこんな感じで
+        # other case
         return redirect(url_for('index'))
