ba694bbde40da70aa667d2d4e1d050bd05eedfd9
kitsystemyou <kitsystemyou@gmail.com>
2020-02-27 06:55:48 +0900

    complete deploy and adjust button

--- application.py
+++ application.py
@@ -3,13 +3,9 @@ import os
 import numpy as np
 
 
-SAVE_DIR = "./static/images"
 SAVE_AUDIO = "./static/audio"
-AZURE_PATH = "/home/site/wwroot/"
+# AZURE_PATH = "/home/site/wwroot/" 
 
-
-if not os.path.isdir(SAVE_DIR):
-    os.mkdir(SAVE_DIR)
 
 if not os.path.isdir(SAVE_AUDIO):
     os.mkdir (SAVE_AUDIO)
@@ -32,14 +28,13 @@ def result():
         rawaudio = request.files['uploadFile']
         savepath = SAVE_AUDIO + "/" + name + ".mp3"
         rawaudio.save(savepath)
-        print("get sound")
+        print("save sound")
         print(savepath)
         print("check path1:" + str(os.path.exists(savepath)))
         os.system('python -m spleeter separate -i ' + savepath +  ' -p spleeter:2stems -o ' \
                     + SAVE_AUDIO)
         print("check path2:" + str(os.path.exists(savepath)))
-        print(os.path.exists(AZURE_PATH + "static/audio/" + name + "/vocals.wav"))
-        print(AZURE_PATH + "static/audio/" + name + "/vocals.wav")
+        print(os.path.exists("./static/audio/" + name + "/vocals.wav"))
 
         return render_template('./result.html', title='Audio separation', name=name)
     
